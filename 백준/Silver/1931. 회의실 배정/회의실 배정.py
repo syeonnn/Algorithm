72424e251d40693fc72c4d703704d08ba49baa7e
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-04-16 17:03:54 +0900

    [Silver I] Title: 회의실 배정, Time: 4112 ms, Memory: 58284 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,17 +1,17 @@
 N = int(input())  # 회의의 수 N
-timetable = []
 
+timetable = []
 for _ in range(N):
   timetable.append(list(map(int, input().split())))
 
 # 끝나는 시간, 시작하는 시간 순으로 정렬
 timetable.sort(key=lambda x : (x[1],x[0]))
-answer = 0
-end_table = 0
+cnt = 1
+end_table = timetable[0][1]
 
-for new_start, new_end in timetable:
-    if end_table <= new_start:
-        answer += 1
-        end_table = new_end
-        
-print(answer)
+for start, end in timetable[1:]:
+  if end_table <= start:    
+    end_table = end
+    cnt += 1
+    
+print(cnt)  # 최대 사용할 수 있는 회의의 최대 개수
