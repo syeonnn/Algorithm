e5c0980921c5e5d5776b989c9aa6d6f7677e6fb3
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-05-03 16:04:51 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,17 @@
+import math
+
+def solution(brown, yellow):
+    answer = []
+    
+    # 중앙 노란색, 테두리 1줄 갈색
+    for i in range(1, int(math.sqrt(yellow))+1):
+        if yellow % i == 0:
+            yw = yellow//i
+            yh = i
+            # 
+            if (yw*2 + yh*2 + 4) == brown:
+                answer.append(yw +2)
+                answer.append(yh +2)
+                break
+                
+    return answer
