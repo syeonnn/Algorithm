4fd322c509ab9f6f0130a6280b3150c06fda12b2
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-05-08 15:35:29 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 72 ms, Memory: 36084 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,17 +1,19 @@
 import sys
+from bisect import bisect_left, bisect_right
 from collections import deque
 input = sys.stdin.readline
 
 N = int(input())
 A = list(map(int, input().split()))
 
-dp = [1 for _ in range(N)]
+dp = [A[0]]
 
 for i in range(N): 
-  for j in range(i):
-    if A[i] > A[j]:
-      dp[i] = max(dp[i], dp[j] + 1)
-      # 수열 A의 i번째 원소에 대해 0~i-1번째 원소들 전체 중에 가장 긴 증가하는 수열을 만들기 위해
-      # 앞에서부터 dp값 구하며 max 비교가 필요함
+  # 맨 마지막 원소보다 현재 위치의 원소가 더 크다면
+  if dp[-1] < A[i]:
+    dp.append(A[i])
+  else:
+    idx = bisect_left(dp,A[i])
+    dp[idx] = A[i]
 
-print(max(dp))
+print(len(dp))  # 가장 긴 증가하는 부분 수열의 길이
