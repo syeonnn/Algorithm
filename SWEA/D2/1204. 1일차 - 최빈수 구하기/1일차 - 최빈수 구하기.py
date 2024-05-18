4494f8a715ccdfd7dd4cbbf4202cc329c899c97b
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-05-18 17:34:11 +0900

    Update 1일차 - 최빈수 구하기.py

--- SWEA/D2/1204. 1일차 - 최빈수 구하기/1일차 - 최빈수 구하기.py
+++ SWEA/D2/1204. 1일차 - 최빈수 구하기/1일차 - 최빈수 구하기.py
@@ -1,1 +1,13 @@
+T = int(input())
+for t in range(1, T + 1):
+    N = int(input())
+    array = list(map(int, input().split()))
+    answer = [0 for _ in range(101)]
 
+    for score in array:
+        answer[score] += 1
+
+    # 조건에 해당하는 모든 값 반환(index함수 역할 업그레이드)
+    max_idxs = [i for i, value in enumerate(answer) if value == max(answer)] 
+
+    print(f'#{N} {max_idxs[-1]}')
