6afc80ee0b3c2d490f19264366497e470fb73054
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-05-25 18:12:29 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,8 +1,7 @@
--- 코드를 입력하세요
-SELECT I.ANIMAL_ID , I.NAME
+-- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회
+-- 보호 시작일이 빠른 순으로 조회
+SELECT I.ANIMAL_ID, I.NAME
 FROM ANIMAL_INS I INNER JOIN ANIMAL_OUTS O
 ON I.ANIMAL_ID = O.ANIMAL_ID
--- 보호 시작일보다 입양일이 더 빠른 동물
 WHERE I.DATETIME > O.DATETIME
--- 보호 시작일이 빠른 순으로 조회
 ORDER BY I.DATETIME
