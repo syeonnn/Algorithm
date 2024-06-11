d0e05f4a999d830eb053e201a79708fbea526e06
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-06-11 14:24:13 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -3,13 +3,10 @@ import math
 def solution(brown, yellow):
     answer = []
     
-    # 중앙 노란색, 테두리 1줄 갈색
     for i in range(1, int(math.sqrt(yellow))+1):
         if yellow % i == 0:
-            yw = yellow // i
             yh = i
-            if (yw*2 + yh*2 + 4) == brown:
-                answer.append(yw +2)
-                answer.append(yh +2)
-                return answer
-                
+            yw = yellow // i
+            if brown == (yh+yw)*2 + 4:
+                answer = [yw+2, yh+2]
+    return answer
