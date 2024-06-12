c068431c39791ace18ae7c2aa28853b6f5854504
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-06-13 00:46:01 +0900

    [level 2] Title: 피로도, Time: 0.08 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,28 +1,21 @@
 from itertools import permutations
-    
+
 def solution(k, dungeons):
     # k: 현재 피로도have
     # dungeons: 최소 필요 피로도need, 소모 피로도cost 
     # 던전 방문 순서 조정으로 유저가 탐험할수 있는 최대 던전 수 구하기
-    answer = -1               
     
-    permutationList = permutations(dungeons)
+    arr = permutations(dungeons) # 던전의 순서 배치에 따라 다른 조합들
+    ans = -1
     
-    for p in (permutationList):
+    for a in arr: # 해당 순서의 조합에 대해
         have = k
-        count = 0
-        
-        for need, cost in p:
-            # 가질 수 있는 최대값이 나오면 for루프 종료
-            if answer == len(dungeons):
-                break
-                
-            if have >= need:
-                have -= cost
-                count += 1
-            else:
-                answer = max(answer,count)
-                break
-            answer = max(answer,count)
-
-    return answer
+        cnt = 0
+        for d in a: # 던전 탐색
+            if have >= d[0]: # 피로도가 충분하다면
+                have -= d[1] # 피로도 소모
+                cnt += 1
+            
+        ans = max(cnt, ans)
+            
+    return ans
