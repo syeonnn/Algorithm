f657869c352819277d3fcee138884e372619a39e
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-06-14 19:52:45 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,5 +1,5 @@
-# 최소! bfs! 덱큐
 from collections import deque
+
 def solution(begin, target, words):
     answer = 0
     visited = [False]*len(words)
@@ -7,29 +7,34 @@ def solution(begin, target, words):
     if target not in words:
         return answer
     
-    def bfs(begin, target, words):
-        # 비교대상 단어와 알파벳이 1개 차이나는 단어가 1개 이상일 수 있음
-        # ( 해당 단어, 변환순서(횟수) ) 쌍으로 같이 저장해야 함!!
+    # begin -> target
+    # words배열에서 begin과 알파벳 한 개 차이나는 단어들이 1개 이상일 수 있음
+    # 따라서 각 단어까지의 변환횟수를 que에 같이 저장한다.
+    # 통합해서 횟수 계산X 단어별, 케이스별 연산O
+        
+    def bfs():
         que = deque()
         que.append((begin,0))
         
         while que:
-            word,count = que.popleft()
+            word,cnt = que.popleft()
             
-            if target == word:   
-                return count
+            if word == target:
+                return cnt
             
             for i in range(len(words)):
                 if not visited[i]:
-                    temp_count = 0
+                    tcnt = 0
                     for j in range(len(word)):
-                        if word[j]!= words[i][j]:
-                            temp_count+= 1
-
-                    if temp_count == 1:
-                        que.append((words[i],count+1))
+                        if word[j] != words[i][j]:
+                            tcnt += 1
+                        else:
+                            continue
+                    if tcnt == 1: # 한 개의 알파벳만 다를 때
+                        que.append((words[i],cnt+1))
                         visited[i] = True
-        
-    answer = bfs(begin, target, words)
+                    
+        return 0
     
-    return answer
+    answer = bfs()
+    return answer       
