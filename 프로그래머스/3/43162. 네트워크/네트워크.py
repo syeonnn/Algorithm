393338af7e89ac17f1749479f7b4c981b506810c
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-06-14 21:41:48 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,40 +1,19 @@
-# from collections import deque
+from collections import deque
 
 def solution(n, computers):
     
     answer = 0
+    visited = [False for _ in range(n)]
     
-#     def bfs(i):
-#         queue = deque()
-#         queue.append(i)
-        
-#         while queue:
-#             i = queue.popleft()
-#             visited[i]=1
-            
-#             for k in range(n):
-#                 if computers[i][k]==1 and visited[k]==0:
-#                     queue.append(k)
-                    
-                
-#     visited = [0 for j in range(n)]
-    
-#     for i in range(n):
-#         if not visited[i]:
-#             bfs(i)
-#             answer+=1
-
-    def dfs(i):
-        visited[i] = True
+    def dfs(x):
+        visited[x] = True
         for j in range(n):
-            if not visited[j] and computers[i][j]==1:
+            if not visited[j] and computers[x][j] == 1:
                 dfs(j)
-        
-    visited = [False]*(n)
-    
+                
     for i in range(n):
         if not visited[i]:
             dfs(i)
-            answer+=1
+            answer += 1
     
     return answer
