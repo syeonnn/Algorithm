dc8ee90f0dccb22d4ddde613c2421d6366482621
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-06-17 20:44:24 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 792 ms, Memory: 56408 KB -BaekjoonHub

---
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split()) # n행 m열
+graph = [list(map(int, input())) for _ in range(n)]
+# 1로 된 가장 큰 정사각형의 크기 반환
+
+answer = 0
+for i in range(1, n):
+  for j in range(1, m):
+    if graph[i][j] == 1:
+      graph[i][j] = min(graph[i-1][j], graph[i][j-1], graph[i-1][j-1]) + 1
+
+for row in graph:
+    answer = max(max(row), answer)
+
+print(answer**2)
