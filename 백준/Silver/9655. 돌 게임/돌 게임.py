2558b12b19e4df312554ec093b445fe5ec708b54
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-07-02 17:24:49 +0900

    [Silver V] Title: 돌 게임, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,23 +1,22 @@
 N = int(input())
 
-turn = 1 # 1 : 상근, -1 : 창영
-
-# 기냥...
-# while N > 0:
-#   if N == 1:
-#     print("SK" if turn == 1 else "CY")
-#     break
-#   elif N >= 3:
-#     N -= 3
-#   else:
-#     N -= 1
-#   turn *= -1
-
 # dp
-# dp = ['' for _ in range(N+1)] # 돌 1개 ~ N개일 때 각각 승자가 누구인지 저장
+dp = [0] * (1000 + 1) # 돌 1개 ~ N개일 때 각각 턴 수
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 1
+ 
+for n in range(4, N+1):
+    dp[n] = min(dp[n-1], dp[n-3]) + 1
+ 
+if dp[N] % 2 == 1:    # 홀수번째 턴으로 끝나면
+    print('SK')
+else:
+    print('CY')
 
 # dp 규칙 찾아보았을 때 결론, 짝수: CY, 홀수: SK
-if N % 2 == 0:
-  print("CY")
-else:
-  print("SK")
+# if N % 2 == 0:
+#   print("CY")
+# else:
+#   print("SK")
