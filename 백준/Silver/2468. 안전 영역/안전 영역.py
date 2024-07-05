6ce529e4fac7a3300ad5446da39e1118268347d0
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-07-05 19:20:52 +0900

    [Silver I] Title: 안전 영역, Time: 628 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,42 +1,43 @@
-# 안전 영역
+# 2468
+# 물에 잠기지 않는 안전영역의 최대 개수
+from collections import deque
 
-import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)] # N행 N열의 어떤 지역의 높이 정보 그래프
 
-n = int(input()) # 그래프 크기 n*n
-graph = []
-max_limit = 0
-min_limit = 101
-result = 0
+answer = [1]  # 아무 지역도 물에 잠기지 않는 경우
 
-for _ in range(n):
-  data = list(map(int, input().split()))
-  graph.append(data)
-  max_limit = max(max_limit,max(data))
+min_h = 101
+max_h = 0
+for row in graph:
+  min_h = min(min(row), min_h)
+  max_h = max(max(row), max_h)
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
-
-def dfs(x,y,limit):
-  for j in range(4):
-    nx = x + dx[j]
-    ny = y + dy[j]
-    if 0<=nx<n and 0<=ny<n and graph[nx][ny] > limit and visited[nx][ny] is False: # r그래프 범위 내o, 물에 잠기지x, 방문x 경우
-      visited[nx][ny] = True
-      dfs(nx,ny,limit)
-
-for i in range(max_limit):
-  visited = [[False for _ in range(n)] for _ in range(n)]
-  cnt = 0
+def bfs(x,y,height): 
+  que = deque()
+  que.append((x,y))
   
-  for x in range(n):
-    for y in range(n):
-      if visited[x][y] is False and graph[x][y] > i:
-        cnt += 1
-        visited[x][y] = True
-        dfs(x,y,i)
-        
-  result = max(result,cnt)
+  while que:
+    x,y = que.popleft()
+    for dx, dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+      nx, ny = x + dx, y + dy
+      
+      if 0<=nx<N and 0<=ny<N and not visited[nx][ny] and graph[nx][ny] > height:
+        visited[nx][ny] = 1
+        que.append((nx,ny))
 
-print(result)
+# 높이 조절하면서 bfs/dfs 탐색
+for height in range(min_h, max_h):
+  cnt = 0
+  visited = [[0]*N for _ in range(N)]
+  
+  for i in range(N):
+    for j in range(N):
+      if not visited[i][j] and graph[i][j] > height:
+        cnt += 1
+        visited[i][j] = 1
+        bfs(i,j,height)
+        
+  answer.append(cnt) # 높이별 안전영역 정보 저장
+  
+print(max(answer))
