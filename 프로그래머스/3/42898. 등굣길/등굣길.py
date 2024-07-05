3c74b98bcf5a8cee9f2edf4353029f586ce79f74
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-07-05 18:13:03 +0900

    [level 3] Title: 등굣길, Time: 2.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -4,10 +4,13 @@ def solution(m, n, puddles):
     # 각각의 좌표에서 대해 오른쪽과 아래쪽으로만 움직여 집에서 학교까지 갈 수 있는 최단경로의 개수
     dp[1][1] = 1
     
-    for M in range(1,m+1):
-        for N in range(1,n+1):
+    for N in range(1, n+1):
+        for M in range(1, m+1):
             if [M,N] in puddles:
                 continue
+            
+            # 물 웅덩이로 인해 초기화되지 않았을 수 있음
+            # 따라서, dp[N][M] = dp[N-1][M] + dp[N][M-1]는 틀린 코드
             dp[N][M] += dp[N-1][M] + dp[N][M-1]
                      
     return dp[n][m] % 1000000007
