f8111434ad6981ce7a9fb9aab97384d5fc91dcb6
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-07-05 18:08:50 +0900

    [level 3] Title: 등굣길, Time: 2.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,12 +1,13 @@
 def solution(m, n, puddles):
-    # 오른쪽과 아래쪽으로만 움직여 집에서 학교까지 갈 수 있는 최단경로의 개수
-    dp = [[0 for _ in range(m+1)] for _ in range(n+1)] #n행 m열
-    dp[1][1] = 1 
-        
-    for N in range(1, n+1):
-        for M in range(1, m+1):
+    
+    dp = [[0] * (m+1) for _ in range(n+1)]
+    # 각각의 좌표에서 대해 오른쪽과 아래쪽으로만 움직여 집에서 학교까지 갈 수 있는 최단경로의 개수
+    dp[1][1] = 1
+    
+    for M in range(1,m+1):
+        for N in range(1,n+1):
             if [M,N] in puddles:
                 continue
-            dp[N][M] += (dp[N][M-1] + dp[N-1][M]) % 1000000007
-    
-    return dp[n][m]
+            dp[N][M] += dp[N-1][M] + dp[N][M-1]
+                     
+    return dp[n][m] % 1000000007
