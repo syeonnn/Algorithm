63f234c915484b1d9479a102a5f9c6a420b5b214
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-07-15 23:30:28 +0900

    [level 2] Title: 과제 진행하기, Time: 3.01 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -2,11 +2,14 @@ def solution(plans):
     answer = []
     plans_mins = []
     
+    
     # 새로 시작해야 되는 과제와 잠시 멈춰둔 과제가 모두 있다면, 새로 시작해야 하는 과제부터 진행
     # 멈춰둔 과제가 여러 개일 경우, 가장 최근에 멈춘 과제부터 시작
+    
     for i in range(len(plans)):
         h,m = map(int, plans[i][1].split(":"))
-        plans_mins.append([plans[i][0], 60*h + m, int(plans[i][2])])    # 과제명, 시작시간, 소요시간
+        # 과제명, 시작시간, 소요시간
+        plans_mins.append([plans[i][0], 60*h + m, int(plans[i][2])]) 
     # 시작 시간 순서대로 정렬
     plans_mins.sort(key = lambda x: x[1])
     
@@ -27,20 +30,14 @@ def solution(plans):
                 answer.append(name)
                 time += need
             else:
-                # temp.append([name, start, (start + need) - next]) # 과제명, 시작시간, 소요시간(남은)
-                # time = next
-                # break
-                need -= (next - time)
+                need -= (next - time)   # 이 부분 계산에서...완전 빙글뱅글...
                 temp.append([name, start, need])
                 time = next
                 break
             
         temp.append(plans_mins[i])
 
-    # for t in temp[::-1]:
-    #     answer.append(t[0])
-    while temp:
-        current_task = temp.pop()
-        answer.append(current_task[0])
+    for t in temp[::-1]:
+        answer.append(t[0])
         
     return answer   # 끝낸 순서대로 과제 이름
