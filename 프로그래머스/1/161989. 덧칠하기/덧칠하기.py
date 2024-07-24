b084cb75101968f2475839ddb98e3b70512f7a69
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-07-24 17:27:48 +0900

    [level 1] Title: 덧칠하기, Time: 9.94 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -2,20 +2,13 @@ def solution(n, m, section):
     answer = 0
     # 1m 구역 n개, 롤러의 길이 m미터, 페인트를 다시 칠해야 하는 구역 번호 section
     
-    # 한 번에 칠해지는지 확인
-    if section[-1] - section[0] + 1 <= m:
-        return 1
-    
     start = section[0]
-    end = section[0] + m - 1
     
-    for s in section:
-        if s <= end:
+    for num in section:
+        if num - start + 1 <= m:
             continue
         else:
             answer += 1
-            start = s
-            end = start + m - 1
-        
-    
+            start = num
+            
     return answer + 1   # 롤러로 페인트칠해야 하는 최소 횟수
