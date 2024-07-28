3393909ad25daa39cf5557c95f7911fcdaa510ae
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-07-28 20:31:58 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.29 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,22 +1,11 @@
 def solution(data, ext, val_ext, sort_by):
     answer = []
-    data_dict = []
-    
-    for row in data:
-        [code, date, maximum, remain] = row
-        data_temp = {}
-        data_temp['code'] = code
-        data_temp['date'] = date
-        data_temp['maximum'] = maximum
-        data_temp['remain'] = remain
-        data_dict.append(data_temp)
+    # !!반복적으로 등장하는 문자열은 배열, 인덱스 활용하기!!
+    by = ['code', 'date', 'maximum', 'remain']
     
     # data에서 ext 값이 val_ext보다 작은 데이터만 뽑은 후, 
-    extracted_data = [ d for d in data_dict if d[ext] < val_ext]
+    extracted_data = [ row for row in data if row[by.index(ext)] < val_ext]
     # sort_by에 해당하는 값을 기준으로 오름차순으로 정렬
-    extracted_data.sort(key = lambda x : (x[sort_by]))
-    
-    for sorted_data in extracted_data:
-        answer.append(list(sorted_data.values()))
+    answer = sorted(extracted_data, key = lambda x : (x[by.index(sort_by)]))
 
     return answer
