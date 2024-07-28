aa02002003ee095ed257d2a198d1cc5ee626b0fc
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-07-28 20:39:26 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 2.98 ms, Memory: 11 MB -BaekjoonHub

---
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -0,0 +1,15 @@
+def solution(s):
+    answer = []
+    temp = {}
+    
+    # 자신의 앞에 같은 글자가 없다면 -1로 표현
+    # 자신보다 앞에 나왔으면서, 자신과 가장 가까운 곳에 있는 같은 글자가 몇 칸 앞에 있는지 표현
+    for idx, ch in enumerate(s):
+        if ch not in temp.keys():
+            temp[ch] = idx
+            answer.append(-1)
+        else:
+            answer.append(idx - temp[ch])
+            temp[ch] = idx
+    
+    return answer
