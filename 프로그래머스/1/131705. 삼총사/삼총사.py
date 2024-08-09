5c766ddfd92516b9dfb0579a5ffa58baf93f87e1
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-08-09 17:47:28 +0900

    [level 1] Title: 삼총사, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,12 +1,13 @@
-import itertools
-
 def solution(number):
     answer = 0
     # 3명의 정수 번호를 더했을 때 0이 되면 '삼총사'!
     # 삼총사를 만들 수 있는 방법의 수
+    l = len(number)
     
-    for element in itertools.combinations(number, 3):
-        if sum(element) == 0:
-            answer += 1
-        
+    for i in range(0, l-2):
+        for j in range(i+1, l-1):
+            for k in range(j+1, l):
+                if number[i] + number[j] + number[k] == 0:
+                    answer += 1
+     
     return answer
