d9a9b3afb73983d57580ac19cdb1ce77fa175995
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-08-11 16:01:14 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,20 +1,21 @@
-from collections import Counter
 
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
 def solution(lottos, win_nums):
-    answer = []
-    rank = {6:1, 5:2, 4:3, 3:4, 2:5, 1:6, 0:6}
 
-    arr = Counter(lottos)
-    cnt0 = arr[0]
-    temp = 0 # 알아볼 수 있는 번호들 중 당첨번호와 같은 번호 개수
-    for num in win_nums:
-        temp += arr[num]
-    
-    if cnt0 == 0:
-        answer = [rank[temp], rank[temp]]
-    else:
-        high = temp + cnt0 if temp <= 6-cnt0 else 6
-        low = temp
-        answer = [rank[high], rank[low]]
-    
-    return answer # 당첨 가능한 최고 순위와 최저 순위
+    rank=[6,6,5,4,3,2,1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+    return rank[cnt_0 + ans],rank[ans]
