265aa7dbdc30f63d31d2f6cc7d76a5241eb07d1e
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-08-11 15:36:00 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,15 +1,19 @@
 def solution(s):
-    answer = ''
     numbers = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
     
-    temp = ''
-    for a in s:
-        if a.isdigit():
-            answer += a
-        else:
-            temp += a
-            if temp in numbers:
-                answer += numbers[temp]
-                temp = ''
+    answer = s
+    for key, value in numbers.items():
+        answer = answer.replace(key, value)
+        
+    # answer = ''
+    # temp = ''
+    # for a in s:
+    #     if a.isdigit():
+    #         answer += a
+    #     else:
+    #         temp += a
+    #         if temp in numbers:
+    #             answer += numbers[temp]
+    #             temp = ''
 
     return int(answer)
