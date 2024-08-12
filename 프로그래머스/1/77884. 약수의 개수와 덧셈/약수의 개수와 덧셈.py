166ef22ec93f1c5ee12e49ab1a9affe694dfb2af
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-08-12 10:10:28 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.86 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -2,13 +2,18 @@ def solution(left, right):
     answer = 0
     
     for i in range(left, right+1):
-        temp = 0
-        for j in range(1, i+1):
-            if i % j == 0:  # 약수이면
-                temp += 1
-        if temp % 2 == 0:
-            answer += i
-        else:
+        if int(i**0.5) == i**0.5:
             answer -= i
+        else:
+            answer += i
+            
+        # temp = 0
+        # for j in range(1, i+1):
+        #     if i % j == 0:  # 약수이면
+        #         temp += 1
+        # if temp % 2 == 0:
+        #     answer += i
+        # else:
+        #     answer -= i
                 
     return answer
