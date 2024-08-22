2918f912aeb50ea103ec8e8079d5e97879958413
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2024-08-23 01:42:33 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 49.78 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,7 +1,16 @@
 from collections import Counter
 
 def solution(participant, completion):   
-
-    answer = Counter(participant) - Counter(completion)
+    temp = {}
+    temp_sum = 0
     
-    return list(answer.keys())[0] # 완주하지 못한 선수의 이름
+    # 해시함수 활용
+    for part in participant:
+        temp[hash(part)] = part
+        temp_sum += hash(part)
+    for comp in completion:
+        temp_sum -= hash(comp)
+        
+    answer = temp[temp_sum]
+    
+    return answer # 완주하지 못한 선수의 이름
