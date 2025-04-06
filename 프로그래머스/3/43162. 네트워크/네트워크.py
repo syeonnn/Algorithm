a4376764f8105d056de3830fc9e7b55bfd42caaf
Seoyeon Park <68735700+syeonnn@users.noreply.github.com>
2025-04-06 16:29:16 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,19 +1,17 @@
-from collections import deque
-
 def solution(n, computers):
-    
     answer = 0
     visited = [False for _ in range(n)]
     
     def dfs(x):
         visited[x] = True
+        
         for j in range(n):
             if not visited[j] and computers[x][j] == 1:
                 dfs(j)
-                
+            
     for i in range(n):
         if not visited[i]:
             dfs(i)
             answer += 1
-    
+        
     return answer
